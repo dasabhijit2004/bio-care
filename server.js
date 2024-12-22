5c18aa3b36bc8c3cd831812660f8047a97bc1c27
Abhijit Das <sadabhijit2004@gmail.com>
2024-12-23 00:54:38 +0530

    Updated

--- server.js
+++ server.js
@@ -58,14 +58,22 @@ const limiter = rateLimit({
 app.use(limiter);
 
 //Connect to MongoDB
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log("Connected to MongoDB");
-}).catch((err) => {
-  console.error("MongoDB connection error:", err);
-});
+// mongoose.connect(MONGO_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// }).then(() => {
+//   console.log("Connected to MongoDB");
+// }).catch((err) => {
+//   console.error("MongoDB connection error:", err);
+// });
+
+const dbconnect = async () => {
+  await connect('<MongoDB_URI>');
+  console.log("Connected to Database");
+}
+
+dbconnect()
+  .catch((err) => console.error(err))
 
 mongoose.connection.on("connected", () => {
   console.log("Mongoose connected to the database");
